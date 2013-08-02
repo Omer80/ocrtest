91bd45c89480acd305e05df04e897b80a390e9b8
Roman Podlinov <r@thisisagile.com>
2013-08-02 15:43:53 -0400

    Cut images script

--- cut_images.py
+++ cut_images.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from scipy.misc import imread
 from skimage.io import imsave
+from skimage.color import rgb2gray
 from numpy.lib.stride_tricks import as_strided
 
 
@@ -33,26 +34,40 @@ def read_images_in_dir(dir, load_pattern = '.png:.jpg:.jpeg:.gif'):
 
     return matches
 
+
+def save_sub_images(image, filename = 'image_%04d.jpg', h_size = 3, v_size = 3):
+    i = 0
+    for r in range(0, image.shape[0], v_size):
+        for c in range(0, image.shape[1], h_size):
+            if (c + h_size ) > image.shape[1]:
+                if (r + v_size ) > image.shape[0]:
+                    imsave(filename % i, image[image.shape[0] - v_size:, image.shape[1] - h_size:])
+                else:
+                    imsave(filename % i, image[r:r + v_size, image.shape[1] - h_size:])
+            else:
+                if (r + v_size ) > image.shape[0]:
+                    imsave(filename % i, image[image.shape[0] - v_size:, c:c + h_size])
+                else:
+                    imsave(filename % i, image[r:r + v_size, c:c + h_size])
+            i +=1
+
+
 def main():
     in_dir = './in/'
     out_dir = './out/'
 
-    # image_files = read_images_in_dir(in_dir)
-    # print image_files
-    #
-    # for filename in image_files:
-    #     im = imread(os.path.join(in_dir, filename), as_grey= True)
-    #     all_windows = rolling_window_2d(im, 30, 30)
-    #     n = 0
-    #     for window in all_windows
-    #
-    #     imsave(os.path.join(out_dir,'image%06d.jpg' % i), im)
-    #     n += 1
+    # A = np.arange(120).reshape((10, 12))
+    # print A
+    # save_sub_images(A, out_dir+'image_%04d.jpg')
 
-    A = np.arange(100).reshape((10, 10))
-    print A
-    A.reshape((3,3))
-    print A
+    image_files = read_images_in_dir(in_dir)
+    for filename in image_files:
+        print filename.split('.')[0] + '_%04d.jpg'
+        # im = imread(os.path.join(in_dir, filename), as_grey= True)
+        im = imread(os.path.join(in_dir, filename))
+        im = rgb2gray(im)
+        save_sub_images(im, out_dir + filename.split('.')[0] + '_%04d.jpg', 30, 30)
+
 
 
 if __name__ == "__main__":

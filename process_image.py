186acbd706f17d9236f2d88d4034d2f753083df4
valeriy <tasman.main@gmail.com>
2013-08-28 00:10:03 +0300

    Fix situation when we test image and don't know where tag is

--- process_image.py
+++ process_image.py
@@ -39,15 +39,17 @@ class Image(object):
         if imsh[0] % self.shiftSize[0] != 0:
             missingRows = self.shiftSize[0] - (imsh[0] % self.shiftSize[0])
             self.image = np.vstack([np.reshape(np.zeros(missingRows * imsh[1]), (missingRows, imsh[1])), self.image])
-            t = self.tagPosition
-            self.tagPosition = (t[0] + missingRows, t[1], t[2] + missingRows, t[3])
+            if self.tagPosition:
+                t = self.tagPosition
+                self.tagPosition = (t[0] + missingRows, t[1], t[2] + missingRows, t[3])
 
         imsh = self.image.shape
         if imsh[1] % self.shiftSize[1] != 0:
             missingColumns = self.shiftSize[1] - (imsh[1] % self.shiftSize[1])
             self.image = np.hstack([np.reshape(np.zeros(missingColumns * imsh[0]), (imsh[0], missingColumns)), self.image])
-            t = self.tagPosition
-            self.tagPosition = (t[0], t[1] + missingColumns, t[2], t[3] + missingColumns)
+            if self.tagPosition:
+                t = self.tagPosition
+                self.tagPosition = (t[0], t[1] + missingColumns, t[2], t[3] + missingColumns)
 
     def extractFeatures(self, positiveImageTemplate=None):
         windowSize, shiftSize, tagPosition = self.windowSize, self.shiftSize, self.tagPosition

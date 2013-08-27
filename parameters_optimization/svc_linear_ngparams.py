87f1ef595063fecea116c7b5ded07bd3200bde53
valeriy <tasman.main@gmail.com>
2013-08-27 16:36:31 +0300

    Linear SVC grid search with parameters from Andrew Ng

--- parameters_optimization/svc_linear_ngparams.py
+++ parameters_optimization/svc_linear_ngparams.py
@@ -5,7 +5,7 @@ from parameters_optimization.optimize_classifier import MetaOptimizer
 
 
 class SVCLinearNgMetaOptimizer(MetaOptimizer):
-    name = 'SVC with linear kernel'
+    name = 'SVC with linear kernel with Andrew Ng recommended parameters'
     classifierFactory = SVC
     grid_parameters = {'C': [0.01, 0.03, 0.1, 0.3, 1, 3, 10], 'kernel': ['linear']}
 

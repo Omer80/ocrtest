897fbc75db2292350cb71034a1906ce51e2659db
valeriy <tasman.main@gmail.com>
2013-09-01 00:02:24 +0300

    Random forest parameters for searching

--- parameters_optimization/randomforest.py
+++ parameters_optimization/randomforest.py
@@ -8,23 +8,23 @@ class RandomForestMetaOptimizer(MetaOptimizer):
     name = 'RandomForest'
     classifierFactory = RandomForestClassifier
     grid_parameters = {
-        'n_estimators': [10, 50, 100, 300, 500],
-        'gamma': [0.001, 0.0001],
+        'n_estimators': [10, 30, 100, 300, 500],
         'criterion': ['gini', 'entropy'],
-        'max_features': ['sqrt', 'log2', None]
+        'max_features': ['sqrt', 'log2', None],
     }
 
     randomized_parameters = {
-        'C': scipy.stats.expon(scale=100),
-        'gamma': scipy.stats.expon(scale=.1),
-        'kernel': ['rbf'],
-        'class_weight': ['auto', None]
+        'n_estimators': [20],
+        'bootstrap': [True]*3 +[False],
+        'criterion': ['gini']*2 + ['entropy'],
+        'max_features': ['sqrt']*3 + ['log2', None],
+        'max_depth': [None]*3 + range(2, 10),
+        'min_samples_split': [2]*3 + range(3, 8, 2),
+        'min_samples_leaf': [1]*2 + range(2, 11, 2)
     }
     iterations = 20
 
     pso_parameters_restrictions = {
-        'C': lambda c: c > 0,
-        'gamma': lambda gamma: gamma >= 0
     }
 
 

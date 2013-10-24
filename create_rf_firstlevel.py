0fbf3941407bc2ae90a4679f23f94cf75af1cf34
valeriy <tasman.main@gmail.com>
2013-10-24 14:40:09 +0300

    Add positive example weight to parameters

--- create_rf_firstlevel.py
+++ create_rf_firstlevel.py
@@ -12,27 +12,31 @@ from ocr_utils import loadDataset
 def test_classifier(classifier, trainData, trainLabel, testData, testLabel, weights=None):
     classifier.fit(trainData, trainLabel, weights)
     testPredicted = classifier.predict(testData)
-    print 'Accuracy: ', metrics.zero_one_score(testLabel, testPredicted)
+    print 'Accuracy: ', metrics.accuracy_score(testLabel, testPredicted)
     print 'F1-score: ', metrics.f1_score(testLabel, testPredicted)
     print metrics.classification_report(testLabel, testPredicted)
 
     return classifier
 
 
-def prepareDatasetWeights(trainY):
-    weights = np.array(trainY) * 2
+def prepareDatasetWeights(trainY, positiveWeight=3):
+    weights = np.array(trainY) * 4
     weights += 1
     return weights
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) < 4:
-        print 'USAGE:\n\t' + sys.argv[0] + ' train.csv test.csv classifier.pkl [estimatorsCount=30]'
+        print 'USAGE:\n\t' + sys.argv[0] + ' train.csv test.csv classifier.pkl [estimatorsCount=30 [positiveWeight=3]]'
         sys.exit(1)
 
     n_estimators = 30
     if len(sys.argv) >= 5:
         n_estimators = int(sys.argv[4])
+
+    positiveWeight = 3
+    if len(sys.argv) >= 6:
+        positiveWeight = int(sys.argv[5]) - 1
 
     trainX, trainY = loadDataset(sys.argv[1])
     testX, testY = loadDataset(sys.argv[2])
@@ -40,6 +44,6 @@ if __name__ == '__main__':
     # test_bunch_of_classifiers(trainX, trainY, testX, testY)
     cl = RandomForestClassifier(**{'n_estimators': n_estimators, 'n_jobs': -1, 'bootstrap': False, 'min_samples_leaf': 1, 'max_features': 'sqrt', 'criterion': 'gini', 'min_samples_split': 2, 'max_depth': None})
     # cl = LogisticRegression()
-    weights = prepareDatasetWeights(trainY)
+    weights = prepareDatasetWeights(trainY, positiveWeight)
     cl = test_classifier(cl, trainX, trainY, testX, testY, weights)
     joblib.dump(cl, sys.argv[3])
